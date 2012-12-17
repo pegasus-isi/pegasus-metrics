078f00ae743c2e16df76653090298ba56b277caf
Gideon Juve <gideon@isi.edu>
2012-12-17 14:23:03 -0800

    Use default argument for StreamHandler (which is what want, sys.stderr) because they changed the name of the keyword argument in 2.7

--- pegasus/metrics/__init__.py
+++ pegasus/metrics/__init__.py
@@ -4,7 +4,7 @@ import logging
 def init_logging():
     logFormat = "%(asctime)s %(levelname)s %(filename)s:%(lineno)s %(message)s"
     logFormatter = logging.Formatter(fmt=logFormat)
-    logHandler = logging.StreamHandler(stream=sys.stderr)
+    logHandler = logging.StreamHandler()
     logHandler.setFormatter(logFormatter)
     log = logging.getLogger(__name__)
     log.addHandler(logHandler)
