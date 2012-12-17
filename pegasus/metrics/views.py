027ff7dd8df4f867bc78e5b4f09b518859d0dad4
Gideon Juve <gideon@isi.edu>
2012-12-17 10:33:52 -0800

    Add configuration, database access

--- pegasus/metrics/views.py
+++ pegasus/metrics/views.py
@@ -4,19 +4,56 @@ except ImportError:
     import simplejson as json
 import logging
 import time
-from flask import request, render_template, redirect, url_for
+from flask import request, render_template, redirect, url_for, g
+import MySQLdb as mysql
 
 from pegasus.metrics import app
 
-MAX_CONTENT_LENGTH = 10*1024
+MAX_CONTENT_LENGTH = 16*1024
+
+def connect_db():
+    return mysql.connect(
+        host=app.config["DBHOST"],
+        user=app.config["DBUSER"],
+        passwd=app.config["DBPASS"],
+        db=app.config["DBNAME"])
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    g.db.close()
+
+def store_json_data(data):
+    cur = g.db.cursor()
+    try:
+        cur.execute("INSERT INTO json_data (data) VALUES (%s)", 
+                [json.dumps(data)])
+    finally:
+        cur.close()
+
+def count_json_data():
+    cur = g.db.cursor()
+    try:
+        cur.execute("SELECT count(*) FROM json_data")
+        return cur.fetchone()[0]
+    finally:
+        cur.close()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    nrows = count_json_data()
+    return render_template('index.html', nrows=nrows)
 
 @app.route('/status')
 def status():
-    # TODO Perform status checks
+    # Make sure the database is reachable
+    count_json_data()
+    
+    # TODO Perform other status checks
+    
     return "", 200
 
 @app.route('/metrics', methods=["POST"])
@@ -48,12 +85,22 @@ def store_metrics():
         logging.error("Error parsing JSON object: %s", e)
         return "Error parsing JSON object", 400
     
-    # Get the remote IP address
-    data["external_address"] = request.environ["REMOTE_ADDR"]
-    data["timestamp"] = time.time()
+    # TODO Validate required fields
+    if "wf_uuid" not in data:
+        return "wf_uuid missing", 400
+    if "type" not in data:
+        return "type missing", 400
     
-    # TODO Store the data
-    #json.dump(data, sys.stderr)
+    # Get the remote IP address
+    data["remote_addr"] = request.environ["REMOTE_ADDR"]
+    data["ts"] = time.time()
+    
+    # Store the data
+    try:
+        store_json_data(data)
+    except Exception, e:
+        logging.error("Error storing JSON data: %s", e)
+        return "Error storing JSON data", 500
     
     return "", 202
 
