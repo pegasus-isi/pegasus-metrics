6dc5f6eb937c51e9bf8543ecc3920cd995ef324c
Rajiv Mayani <mayani@isi.edu>
2022-04-08 13:44:37 -0700

    Set API keys as config. param

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
-HOST = '0.0.0.0'
+HOST = "0.0.0.0"
 PORT = 5000
-DBHOST = 'localhost'
+DBHOST = "localhost"
 DBPORT = 3306
-DBUSER = 'pegasus'
-DBPASS = 'pegasus'
-DBNAME = 'pegasus_metrics'
+DBUSER = "pegasus"
+DBPASS = "pegasus"
+DBNAME = "pegasus_metrics"
 DEBUG = False
-LOGLEVEL = 'INFO'
-LOGFORMAT = "%(asctime)-15s %(levelname)s %(name)s (%(filename)s:%(lineno)s) %(message)s"
+LOGLEVEL = "INFO"
+LOGFORMAT = (
+    "%(asctime)-15s %(levelname)s %(name)s (%(filename)s:%(lineno)s) %(message)s"
+)
+GOOG_API_KEY = "AIzaSyDtSkfKqWlDQ0Qd3WNmbAxfCAS3zkw_W7g"
